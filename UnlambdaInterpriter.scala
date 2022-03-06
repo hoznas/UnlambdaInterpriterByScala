a8b1739b3005a504128d9c26361727beaccfedb6
unkun <purupuru.soda@gmail.com>
2022-03-06 16:17:22 +0900

    new class(single/multi function)

--- UnlambdaInterpriter.scala
+++ UnlambdaInterpriter.scala
@@ -3,75 +3,58 @@ import scala.util.matching.Regex
 abstract class UnlambdaObject{
   def toString(): String
 }
+////////////////////////////////////////////////////////
 trait Node{
   def eval(): Function
-
 }
 
 class Apply(fun: Node, arg: Node) extends UnlambdaObject with Node{
-  override def toString(): String = "(%s %s)".format(fun, arg)
-  override def eval(): Function ={
+  def eval(): Function ={
     val ef = fun.eval()
     val ea = arg.eval()
     ef.call(ea)
   }
+  override def toString(): String = "(%s %s)".format(fun, arg)
 }
-
-
-abstract class Function extends UnlambdaObject with Node{
+/////////////////////////////////////////////////////////
+trait Function {
   def call(arg:Function): Function
-  def eval(): Function
+  def eval(): Function = this
 }
-class I(name: String="I") extends Function {
-  def call(arg: Function): Function = arg
-  override def toString():String = name
-  override def eval():Function = this
-}
-class K(name: String="K") extends Function{
+
+class SingleArgFunction(name: String) extends UnlambdaObject with Function with Node {
   def call(arg: Function): Function = {
-    new K2(arg)
-  }
-  override def toString():String = name
-  override def eval():Function = this
-}
-class K2(x: Function,name: String="K2") extends Function{
-    def call(arg: Function): Function = {
-      x
+    val re_dot: Regex = """^\.(.)""".r
+    name match {
+      case "i" => arg
+      case "k" => new MultiArgFunction("K2", arg, this) // "this" is dummy
+      case "s" => new MultiArgFunction("S2", arg, this) // "this" is dummy
+      case "r" => 
+        print("\n")
+        arg
+      case re_dot(x) => 
+        print(x)
+        arg
+      case _ => throw new Exception("SingleArgFunction("+name+") => ERROR")
     }
-  override def toString():String = name
-  override def eval():Function = this
-}
-class S(name: String="S") extends Function{
-  def call(arg: Function): Function = {
-    new S2(arg)
   }
   override def toString():String = name
-  override def eval():Function = this
 }
-class S2(x:Function,name: String="S2") extends Function {
+class MultiArgFunction(name:String, x:Function, y:Function) extends UnlambdaObject with Function with Node {
   def call(arg: Function): Function = {
-    new S3(x,arg)
-  } 
-  override def toString():String = name
-  override def eval():Function = this
-}
-
-class S3(x:Function, y:Function,name: String="S3") extends Function{
-  def call(arg: Function): Function = {
-    x.call(arg).call(y.call(arg))
+    name match {
+      case "K2" => this.x
+      case "S2" => new MultiArgFunction("S3",x,arg)    
+      case "S3" => 
+        val xx = x.call(arg)
+        val yy = y.call(arg)
+        xx.call(yy)
+      case _ => throw new Exception("MultiArgFunction("+name+") => ERROR")
+    }
   }
   override def toString():String = name
-  override def eval():Function = this
 }
-
-class Dot(name: String) extends Function {
-  def call(arg: Function): Function = {
-    print(name)
-    arg
-  }
-  override def toString():String = "." + name
-  override def eval():Function = this
-}
+/////////////////////////////////////////////////////////
 object UnlambdaInterpriter{
   def tokenize(src: String, result: List[String] = Nil): Array[String] = {
     if(src == "") return result.reverse.toArray
@@ -80,12 +63,9 @@ object UnlambdaInterpriter{
     val re_dot: Regex = """^(\..)(.*)""".r
 
     val (inst:String,tail:String) = src match {
-      case re_single_char(x,y) =>
-        (x,y)
-      case re_dot(x,y) =>
-        (x,y)
-      case _ =>
-        return tokenize(src.tail, result)
+      case re_single_char(x,y) => (x,y)
+      case re_dot(x,y)         => (x,y)
+      case _                   => return tokenize(src.tail, result)
     }
     tokenize(tail, inst::result)
   }
@@ -102,16 +82,13 @@ object UnlambdaInterpriter{
     if(tr.eos()) throw new Exception("make_node() => EOS")
     val t = tr.next()
     val re_dot: Regex = """^\.(.)""".r
-    val result = t match {
+    val result:Node = t match {
       case "`" => 
         val fun = make_node(tr)
         val arg = make_node(tr)
         new Apply(fun,arg)
-      case "i" => new I()
-      case "k" => new K()
-      case "s" => new S()
-      case "r" => new Dot("\n")
-      case re_dot(x) =>  new Dot(x)
+      case "i" | "k" | "s" | "r"=> new SingleArgFunction(t)
+      case re_dot(x) =>  new SingleArgFunction(t)
       case _   => throw new Exception("make_node() => unknown function[" + t+"]")
     }
     return result
@@ -122,8 +99,9 @@ object UnlambdaInterpriter{
     val src = if(args.length != 0 ) args(0) 
               else "`r```````````.H.e.l.l.o. .w.o.r.l.di"
 
+    println("[SOURCE] => " + src)
     val tokens = tokenize(src) 
-    println("[TOKENS] => " + tokens.mkString("","",""))
+    println("[TOKENS] => " + tokens.mkString(""," ",""))
     val node = make_node(new TokenReader(tokens))
     println("[NODES] => "+node)
     println("=====eval=====")
